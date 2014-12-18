28cf0e6d50c1ab9087445306e053494d761a7673
jiaoxiaoting158 <jiaoxiaoting_158@qq.cm>
2014-12-18 15:12:15 +0800

    add project

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by jiaoxt on 12/18/14.
+//  Copyright (c) 2014 jiaoxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
